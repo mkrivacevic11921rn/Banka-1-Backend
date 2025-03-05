b1b3143c1437260d9aad72ffe55d51b739e181be
Nemanja02 <nemanja02marjanov@gmail.com>
2025-03-05 18:10:03 +0100

    cors ispravka

--- user-service/src/main/java/com/banka1/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/banka1/user/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
