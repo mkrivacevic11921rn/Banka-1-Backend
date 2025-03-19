d0661b0cb65eafbfdee7226d57795b0c22450f06
lkukolj2621rn <lkukolj2621rn@raf.rs>
2025-03-19 02:42:17 +0100

    Kreiranje i pregled naloga

--- trading-service/go.mod
+++ trading-service/go.mod
@@ -8,12 +8,16 @@ require (
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/glebarez/go-sqlite v1.22.0 // indirect
 	github.com/glebarez/sqlite v1.11.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.25.0 // indirect
 	github.com/gofiber/fiber/v2 v2.52.6 // indirect
 	github.com/gofiber/swagger v1.1.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -22,6 +26,7 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -35,6 +40,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.59.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
 	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
---
+++ trading-service/orders/Orders.go
@@ -0,0 +1,244 @@
+package orders
+
+import (
+	"strings"
+
+	"banka1.com/db"
+	"banka1.com/types"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+var validate = validator.New(validator.WithRequiredStructEnabled())
+
+func OrderToOrderResponse(order types.Order) types.OrderResponse {
+	return types.OrderResponse{
+		ID: order.ID,
+		UserID: order.UserID,
+		SecurityID: order.SecurityID,
+		OrderType: order.OrderType,
+		Quantity: order.Quantity,
+		ContractSize: order.ContractSize,
+		PricePerUnit: order.PricePerUnit,
+		Direction: order.Direction,
+		Status: order.Status,
+		ApprovedBy: order.ApprovedBy,
+		IsDone: order.IsDone,
+		LastModified: order.LastModified,
+		RemainingParts: order.RemainingParts,
+		AfterHours: order.AfterHours,
+	}
+}
+
+func GetOrderByID(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id", -1)
+	if err != nil || id <= 0 {
+		var response types.Response
+		response.Success = false
+		if err != nil {
+			response.Error = "Nevalidan ID: " + err.Error()
+		} else {
+			response.Error = "Nevalidan ID"
+		}
+		return c.Status(400).JSON(response)
+	}
+	var order types.Order
+	if err := db.DB.First(&order, id).Error; err != nil {
+		return c.Status(404).JSON(types.Response{
+			Success: false,
+			Error: "Nije pronadjen: " + err.Error(),
+		})
+	}
+	return c.JSON(types.Response{
+		Success: true,
+		Data: OrderToOrderResponse(order),
+	});
+}
+
+func GetOrders(c *fiber.Ctx) error {
+	filterStatus := strings.ToLower(c.Query("filter_status", "all"))
+	var orders []types.Order
+	var err error
+	if "all" == filterStatus {
+		err = db.DB.Find(&orders).Error
+	} else {
+		err = db.DB.Find(&orders, "status = ?", filterStatus).Error
+	}
+	if err != nil {
+		return c.Status(400).JSON(types.Response{
+			Success: false,
+			Error: "Neuspela pretraga: " + err.Error(),
+		})
+	}
+	responses := make([]types.OrderResponse, len(orders))
+	for i, order := range orders {
+		responses[i] = OrderToOrderResponse(order)
+	}
+	return c.JSON(types.Response{
+		Success: true,
+		Data: responses,
+	});
+}
+
+func CreateOrder(c *fiber.Ctx) error {
+	var orderRequest types.CreateOrderRequest
+	if err := c.BodyParser(&orderRequest); err != nil {
+		return c.Status(400).JSON(types.Response{
+			Success: false,
+			Error: "Neuspelo parsiranje: " + err.Error(),
+		})
+	}
+	if err := validate.Struct(orderRequest); err != nil {
+		return c.Status(400).JSON(types.Response{
+			Success: false,
+			Error: "Neuspela validacija: " + err.Error(),
+		})
+	}
+	order := types.Order{
+		UserID: orderRequest.UserID,
+		SecurityID: orderRequest.SecurityID,
+		OrderType: orderRequest.OrderType,
+		Quantity: orderRequest.Quantity,
+		ContractSize: orderRequest.ContractSize,
+		PricePerUnit: orderRequest.PricePerUnit,
+		Direction: orderRequest.Direction,
+		Status: "pending", // TODO: pribaviti needs approval vrednost preko token-a?
+		ApprovedBy: nil,
+		IsDone: false,
+		RemainingParts: &orderRequest.Quantity,
+		AfterHours: false, // TODO: dodati check za ovo
+	}
+	tx := db.DB.Create(&order)
+	if err := tx.Error; err != nil {
+		return c.Status(400).JSON(types.Response{
+			Success: false,
+			Error: "Neuspelo kreiranje: " + err.Error(),
+		})
+	}
+	return c.JSON(types.Response{
+		Success: true,
+		Data: order.ID,
+	});
+}
+
+func ApproveDeclineOrder(c *fiber.Ctx, decline bool) error {
+	id, err := c.ParamsInt("id", -1)
+	if err != nil || id <= 0 {
+		var response types.Response
+		response.Success = false
+		if err != nil {
+			response.Error = "Nevalidan ID: " + err.Error()
+		} else {
+			response.Error = "Nevalidan ID"
+		}
+		return c.Status(400).JSON(response)
+	}
+	var order types.Order
+	if err := db.DB.First(&order, id).Error; err != nil {
+		return c.Status(404).JSON(types.Response{
+			Success: false,
+			Error: "Nije pronadjen: " + err.Error(),
+		})
+	}
+	if order.Status != "pending" {
+		return c.Status(400).JSON(types.Response{
+			Success: false,
+			Error: "Nije na cekanju",
+		})
+	}
+	if decline {
+		order.Status = "declined"
+	} else {
+		// TODO: provera da li je vremenski ogranicen?
+		order.Status = "approved"
+	}
+	order.ApprovedBy = new(uint)
+	*order.ApprovedBy = 0 // TODO: dobavi iz token-a
+	db.DB.Save(&order)
+	return c.JSON(types.Response{
+		Success: true,
+		Data: order.ID,
+	});
+}
+
+func DeclineOrder(c *fiber.Ctx) error {
+	return ApproveDeclineOrder(c, true)
+}
+
+func ApproveOrder(c *fiber.Ctx) error {
+	return ApproveDeclineOrder(c, false)
+}
+
+func InitRoutes(app *fiber.App) {
+	// swagger:operation GET /orders/{id} GetOrderByID
+	//
+	// Pregled naloga po ID-u.
+	//
+	// ---
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: ID naloga
+	//   type: integer
+	//   required: true
+	// responses:
+	//   '200':
+	//     schema:
+	//       $ref: '#/definitions/OrderResponse'
+	app.Get("/orders/:id", GetOrderByID)
+	// swagger:operation GET /orders GetOrders
+	//
+	// Pregled svih naloga, sa filtriranjem po statusu.
+	//
+	// ---
+	// parameters:
+	// - name: filter_status
+	//   in: query
+	//   description: Status za filtriranje, ili "all" (bez filtriranja, to je podrazumevana vrednost)
+	//   default: "all"
+	//   type: string
+	// responses:
+	//   '200':
+	//     schema:
+	//       type: array
+	//       items:
+	//         $ref: '#/definitions/OrderResponse'
+	app.Get("/orders", GetOrders)
+	// swagger:operation POST /orders CreateOrder
+	//
+	// Kreiranje novog naloga
+	//
+	// ---
+	// parameters:
+	// - name: body
+	//   in: body
+	//   description: Podaci o nalogu
+	//   required: true
+	//   schema:
+	//      $ref: '#/definitions/CreateOrderRequest'
+	app.Post("/orders", CreateOrder)
+	// swagger:operation POST /orders/{id}/decline DeclineOrder
+	//
+	// Odbijanje naloga.
+	//
+	// ---
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: ID naloga
+	//   type: integer
+	//   required: true
+	app.Post("/orders/:id/decline", DeclineOrder)
+	// swagger:operation POST /orders/{id}/approve ApproveOrder
+	//
+	// Odobrenje naloga.
+	//
+	// ---
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: ID naloga
+	//   type: integer
+	//   required: true
+	app.Post("/orders/:id/approve", ApproveOrder)
+}
