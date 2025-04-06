e24be754d664638bfc4a3f743ed2f989d9e55907
lkukolj2621rn <lkukolj2621rn@raf.rs>
2025-04-06 17:01:03 +0200

    Dodao mogucnost pravljenja listener-a u trading servisu

--- trading-service/main.go
+++ trading-service/main.go
@@ -110,11 +110,15 @@ func main() {
 		orders.LoadOrders()
 		log.Println("Finished loading default orders")
 	}()
+
 	func() {
 		log.Println("Starting to load default portfolios...")
 		orders.LoadPortfolios()
 		log.Println("Finished loading default portfolios")
 	}()
+
+	broker.StartListeners(ctx)
+
 	app := fiber.New()
 
 	app.Use(func(c *fiber.Ctx) error {
