32247ce37025bf3fefff744f314509e389bc55ef
Nemanja02 <nemanja02marjanov@gmail.com>
2025-04-08 15:52:36 +0200

    notification servis fix

--- notification-service/src/main/java/com/banka1/notification/model/CustomerDevice.java
+++ notification-service/src/main/java/com/banka1/notification/model/CustomerDevice.java
@@ -1,6 +1,8 @@
 package com.banka1.notification.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class CustomerDevice {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String deviceToken;
